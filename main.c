201a79f9e768e319ba4bd9b16fd2ef1be8838e77
NotEvenARobert <132989142+NotEvenARobert@users.noreply.github.com>
2025-09-19 12:25:20 -0400

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,21 @@
+/* COP 3502C PA2
+This program is written by: ANH NHAT LE (Robert) */
 
+#include <stdio.h>
+#include <math.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(void) {
+    
+    char letters[8];
+    int dic;
+    char middle;
+    
+    scanf("%s", letters);
+    scanf("%d", &dic);
+    middle = letters[3];
+
+    return 0;
+}
